f039aeb51ce834850cec045055ea3093e1b7c758
Enas Abu Shanab <enas.abushanab98@gmail.com>
2023-01-04 16:29:41 +0000

    logical operators

---
+++ Apply.php
@@ -0,0 +1,14 @@
+<?php 
+
+$username = "Enas";
+$email = "enas.abushanab98@gmail.com";
+$password = "7777777";
+
+if(($username == "Enas" || $email == "enas.abushanab98@gmail.com") && $password = "7777777"){
+    echo "You signed up correctly";
+} else{
+    echo "Sorry, there was an error";
+}
+
+
+?>
