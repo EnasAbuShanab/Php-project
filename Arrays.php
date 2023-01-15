3f4ee5337d80255b7d91b232bb73b932e33743d7
Enas Abu Shanab <enas.abushanab98@gmail.com>
2023-01-15 17:45:12 +0000

    associative array

--- Arrays.php
+++ Arrays.php
@@ -1,5 +1,5 @@
 <?php
-
+# indexed array
 $fruit = array("Apple", "Orange", "Banana");
 echo $fruit[2];
 
@@ -7,10 +7,23 @@ for ($counter=0; $counter<count($fruit); ++$counter){
     echo "<div>". $fruit[$counter]."</div>";
 }
 
+# foreach loop 
 foreach ($fruit as $food){
 
     echo ".."."<div>". $food."</div>";
  }
 
+# associative array
+$Personal_info= ["name"=> "enas", "age"=>'24', 'city'=>'Amman'];
+foreach ($Personal_info as $info){
 
+    echo ".."."<div>". $info."</div>";
+    
+ }
+foreach ($Personal_info as $info=>$data){
+
+    echo ".."."<div>". $info.' : ' .$data."</div>";
+    
+}
+  echo $Personal_info['age'];
 ?>
